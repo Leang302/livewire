b7d3d0f4d420b4bc1ee77fced75d5534930bbdb6
kimleang <leangk49@gmail.com>
2024-04-01 11:40:21 +0700

    Form validation

--- app/Livewire/Clicker.php
+++ app/Livewire/Clicker.php
@@ -3,16 +3,22 @@
 namespace App\Livewire;
 
 use App\Models\User;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 class Clicker extends Component
 {
+
     //not put something that's s here
+    #[Rule('required|min:3|max:10')]
    public $name;
+   #[Rule('required|email|unique:users')]
    public $email;
+   #[Rule('required|min:3')]
    public $password;
 
  public function createNewUser(){
+    $this->validate();
     User::create([
         'name'=>$this->name,
         'email'=>$this->email,
--- resources/views/livewire/clicker.blade.php
+++ resources/views/livewire/clicker.blade.php
@@ -1,8 +1,17 @@
 <div>
     <form wire:submit.prevent='createNewUser' action="">
         <input wire:model="name" type="text" placeholder="Name">
+        @error('name')
+        <p>{{ $message }}</p>
+        @enderror
         <input wire:model="email" type="email" placeholder="Email">
+        @error('email')
+        <p>{{ $message }}</p>
+        @enderror
         <input wire:model="password" type="password" placeholder="Password">
+        @error('password')
+        <p>{{ $message }}</p>
+        @enderror
         <button type="submit">Create</button>
     </form>
     <hr>
